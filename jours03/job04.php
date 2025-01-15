06fe3a3dfed5a092c931b1fbd7540bc0addde30f
Live Yu Peng Sébastien <sebastien.live-yu-peng@laplateforme.io>
2025-01-15 10:59:46 +0100

    Add files via upload

---
+++ jours03/job04.php
@@ -0,0 +1,12 @@
+<?php
+
+$str = "Dans l'espace, personne ne vous entend crier";
+$charCount = 0;
+
+for ($i = 0; $i < strlen($str); $i++) {
+    $charCount++;
+}
+
+echo "Nombre de caractères = $charCount";
+
+?>
